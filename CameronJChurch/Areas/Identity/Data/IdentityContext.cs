b04515bb5ff8a3541b72e0a66632d50e92f549e4
Cameron <cameron.j.church@live.com>
2021-09-10 14:26:21 -0400

    Add project files.

---
+++ CameronJChurch/Areas/Identity/Data/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using CameronJChurch.Areas.Identity.Data;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CameronJChurch.Data
+{
+    public class IdentityContext : ApiAuthorizationDbContext<CameronJChurchUser>
+    {
+        public IdentityContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ CameronJChurch/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using CameronJChurch.Areas.Identity.Data;
+using CameronJChurch.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CameronJChurch.Areas.Identity.IdentityHostingStartup))]
+namespace CameronJChurch.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<CameronJChurchUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
